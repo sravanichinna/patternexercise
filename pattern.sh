a10fa8ca51414473ee2bb9f622cc3d84a9993c63
sravanichinna <sravanidane413.com>
2020-11-18 20:26:45 +0530

    added: emailpattern logic

--- pattern.sh
+++ pattern.sh
@@ -9,3 +9,12 @@ then
 else
 	echo "no"
 fi
+
+read -p "enter email address:" email
+emailpat="^[0-9a-zA-Z]+([.\-_+][0-9a-zA-z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+if (( $email == $emailpat ))
+then
+	echo "yes"
+else
+	echo "no"
+fi
