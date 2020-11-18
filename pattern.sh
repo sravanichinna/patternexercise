ca873e837ddd880cbd1776649adb5b182aa42b41
sravanichinna <sravanidane413.com>
2020-11-18 20:15:07 +0530

    added: first-lastname logic

--- pattern.sh
+++ pattern.sh
@@ -1,2 +1,11 @@
 #!/bin/bah
 echo"welcome to pattern execise"
+
+read -p "enter first-lastname:" firstlastname
+firstlastnamepat="^[A-Z]{1}[a-z]{2,}[A-z]{1}[a-z]{2,}$"
+if(( $firstlastname == $firstlastnamepat ))
+then 
+	echo "yes"
+else
+	echo "no"
+fi
